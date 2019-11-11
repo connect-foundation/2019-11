0f499f424e7e899e9ae4f659767668dd775ead11
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-11 16:23:55 +0900

    Change server module
    서버 모듈 변경

---
+++ server/server/common/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { Application } from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import http from 'http';
+import os from 'os';
+import cookieParser from 'cookie-parser';
+
+  import installValidator from './swagger';
+
+import l from './logger';
+
+const app = express();
+
+export default class ExpressServer {
+  constructor() {
+    const root = path.normalize(__dirname + '/../..');
+    app.set('appPath', root + 'client');
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}));
+    app.use(cookieParser(process.env.SESSION_SECRET));
+    app.use(express.static(`${root}/public`));
+  }
+
+  router(routes: (app: Application) => void): ExpressServer {
+    installValidator(app, routes)
+    return this;
+  }
+
+  listen(p: string | number = process.env.PORT): Application {
+    const welcome = port => () => l.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname() } on port: ${port}}`);
+    http.createServer(app).listen(p, welcome(p));
+    return app;
+  }
+}
