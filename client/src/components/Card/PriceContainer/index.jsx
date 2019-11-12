2dcd3bbde7f69980219eba8c1726da9ff84d196c
sphong5911 <sphong5911@naver.com>
2019-11-12 21:43:48 +0900

    Update PriceContainer component style
    금액관련 항목들을 세로로 나열하여 보여주기 위하여 display 방식을 flex로
    변경

--- client/src/components/Card/PriceContainer/index.jsx
+++ client/src/components/Card/PriceContainer/index.jsx
@@ -3,12 +3,17 @@ import styled from 'styled-components';
 import BuyNowPrice from '../BuyNowPrice';
 import TopBid from '../TopBid';
 
+const PriceContainerStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const PriceContainer = (props) => {
   return(
-    <>
+    <PriceContainerStyle>
       <BuyNowPrice buyNowPrice={props.buyNowPrice}/>
       <TopBid topBid={props.topBid}/>
-    </>
+    </PriceContainerStyle>
   )
 }
 
