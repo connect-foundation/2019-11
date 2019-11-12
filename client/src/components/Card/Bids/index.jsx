c5b67d75a96e49963643f0e3d93b6f5b5e2498ce
sphong5911 <sphong5911@naver.com>
2019-11-12 21:47:06 +0900

    Update Bids component style
    입찰수에 아이콘을 추가하기 위하여 style 및 이미지 추가

--- client/src/components/Card/Bids/index.jsx
+++ client/src/components/Card/Bids/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const BidsStyle = styled.div``;
+const BidsStyle = styled.div`
+  img {
+    width: 0.8rem;
+    height: 0.8rem;
+  }
+
+  font-size: small;
+`;
 
 const Bids = (props) => {
   return (
     <BidsStyle>
+      <img src="/icon/person.jpg"/>
       {props.bids}
     </BidsStyle>
   )
