6738a9483168b7fad5b325237af0a459f6105c0d
deokisys <sjb378@me.com>
2019-11-13 21:13:54 +0900

    Add trade list page
    거래 내역 페이지 추가
    route를 통해 거래내역 페이지를 연결해준다.

---
+++ client/src/pages/TradeList/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState , useEffect} from 'react';
+import TradeList from '../../components/TradeList';
+function Trade() {
+    return (
+      <>
+        <TradeList/>
+      </>
+    );
+  }
+  
+  export default Trade;
