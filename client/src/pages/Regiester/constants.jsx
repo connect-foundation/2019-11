a9ce5cd2cc7e1327342ba19505280814852158b8
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-14 18:07:09 +0900

    Implement register page
    등록 페이지 구현
    2019/11/14 기준 완성도 약 60%
    몇 Component 추가 및 위치 조정이 필요하다.
    중복되는 코드 제거가 필요하다.
    
    현재 page에서 사용될 상수값을 저장하는 constatns.jsx 추가

---
+++ client/src/pages/Regiester/constants.jsx
@@ -0,0 +1,8 @@
+export const phaseList = [
+    'Step 1. 카테고리',
+    'Step 2. 판매방식',
+    'Step 3. 상품등록',
+    'Step 4. 완료'
+]
+
+export default { phaseList }
--- client/src/pages/Regiester/index.jsx
+++ client/src/pages/Regiester/index.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import Progress from '../../components/RegisterProgress'
+import CategoryPage from './1'
+import SellKindPage from './2'
+import InfoPage from './3'
+import { phaseList } from './constants'
+
+
+const WIDTH = 80;
+
+const Container = styled.div`
+    width: ${WIDTH}rem;
+    height: 100%;
+    overflow-y: auto;
+    box-sizing:border-box;
+    border-right: gray solid 1px;
+    border-left: gray solid 1px;
+    margin: 0 auto;
+`
+
+const Content = styled.div`
+    width: 100%;
+    height: calc(100vh - 10rem);
+    box-sizing: border-box;
+    background-color:whitesmoke;
+    overflow-x:hidden;
+`
+
+const Window = styled.div`
+    display:flex;
+    width: ${phaseList.length * WIDTH}rem;
+    height: 90%;
+
+    transform: ${props => `translateX(${-props.phase * WIDTH}rem)`};
+    transition: transform .2s ease-in-out;
+`
 
 const Page = () => {
+    const [phase, setPhase] = useState(0);
+
     return (
-        <>
-        </>
+        <Container>
+            <Progress phase={phase} list={phaseList}/>
+            <Content>
+                <Window phase={phase}>
+                    <CategoryPage width={80} prev={() => setPhase(0)} next={() => setPhase(1)}/>
+                    <SellKindPage width={80} prev={() => setPhase(0)} next={() => setPhase(2)}/>
+                    <InfoPage width={80} prev={() => setPhase(1)} next={() => setPhase(3)}/>
+                </Window>
+            </Content>
+        </Container>
     )
 }
 
