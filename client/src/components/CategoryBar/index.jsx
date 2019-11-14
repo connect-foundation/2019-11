fda32d3afc56aadeb4893edf790cb1d818661db5
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-14 22:07:58 +0900

    Update expand list in category bar
    카테고리바의 확장 탭 값 조정
    이전에 창을 오른쪽으로 밀어내던 현상 연장선
    absolute로 분리해서 오른쪽 페이지에는 영향 없음

--- client/src/components/CategoryBar/index.jsx
+++ client/src/components/CategoryBar/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Logo from './Logo'
 import CategoryIcon from './CategoryIcon'
@@ -6,22 +6,36 @@ import Cloth from '../../assets/cloth.svg'
 import Electronic from '../../assets/television.svg'
 import ExpandList from './ExpandList'
 
+const WIDTH = 5;
+
 const Container = styled.div`
     display:flex;
+    position:relative;
 `
 
 const OriginWrapper = styled.div`
-    width: 5em;
+    width: ${WIDTH}rem;
     display:flex;
     flex-direction:column;
     z-index: 2;
-    background: white;
-    border-right: #FEF2C7 0.2rem solid;
+    background: #beddbf77;
+    border-right: #FEAA6E 0.2rem solid;
+`
+
+const ListWrapper = styled.div`
+    position:absolute;
+    width: ${props=> props.open ? 15 : 0}rem;
+    height:100%;
+    left: ${WIDTH}em;
+    z-index: 999;
+
+    overflow: hidden;
+    transition: width .35s ease-in-out;
 `
 
 const Bar = styled.div`
     height: 100%;
-    width: 5em;
+    width: 100%;
     overflow-y: auto;
     box-sizing: border-box;
     padding: 0.5em 1em;
@@ -38,32 +52,33 @@ const Components = () => {
     const [open, setOpen] = useState(false);
 
     const handleClick = (event) => setOpen(!open)
-    
 
     return (
         <Container>
             <OriginWrapper>
-            <Logo />
-            <Bar>
-                <List>
-                    <CategoryIcon
-                        color="#FFE1A2"
-                        img={Cloth}
-                        text="의류"
-                        active={open}
-                        onClick={handleClick}
-                    />
-                    <CategoryIcon
-                        color="#BEDDBF"
-                        img={Electronic}
-                        text="가전"
-                        active={open}
-                        onClick={handleClick}
-                    />
-                </List>
-            </Bar>
+                <Logo />
+                <Bar>
+                    <List>
+                        <CategoryIcon
+                            color="#FFE1A2"
+                            img={Cloth}
+                            text="의류"
+                            active={open}
+                            onClick={handleClick}
+                        />
+                        <CategoryIcon
+                            color="#BEDDBF"
+                            img={Electronic}
+                            text="가전"
+                            active={open}
+                            onClick={handleClick}
+                        />
+                    </List>
+                </Bar>
             </OriginWrapper>
-            <ExpandList open={open}/>
+            <ListWrapper open={open} >
+                <ExpandList/>
+            </ListWrapper>
         </Container>
     )
 }
