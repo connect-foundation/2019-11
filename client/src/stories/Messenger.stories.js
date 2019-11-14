b025926cb11196b3bad171a65e3184ca972d6663
deokisys <sjb378@me.com>
2019-11-14 15:24:03 +0900

    Add main show messenger button to storybook
    스토리북에 메신저 메인 버튼 추가

---
+++ client/src/stories/Messenger.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import MessengerButton from '../components/Messenger/MessengerButton';
+
+export default {
+  title: 'Messenger',
+};
+
+
+
+export const MainButton = () => {
+  return(
+      <>
+      <MessengerButton>
+      </MessengerButton>
+      </>
+  )
+}
