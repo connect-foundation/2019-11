051061e234ce8203f303ce182fa0fd5431592714
deokisys <sjb378@me.com>
2019-11-14 18:04:47 +0900

    Add chat view for receve message and send message layout
    메신저에서 메시저 방으로 들어갔을때 보여지는
    메시지 전송과 수신시의 레이아웃을 구현, 추가
    전송 메시지는 연한색, 수신한 메시지는 연한 색으로 표현

---
+++ client/src/components/Messenger/MessengerWrap/MessengerChat/index.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import React, { useState , useEffect} from 'react';
+import MessengerReceive from './MessengerReceive';
+import MessengerSend from './MessengerSend';
+const MessengerScroll = styled.div`
+width:19.5rem;
+height:10rem;
+overflow-x:hidden;
+overflow-y:auto;
+`;
+function MessengerChatWrap(props) {
+
+    return (
+        <MessengerScroll>
+                <MessengerReceive ReceiveText={"test입니다. 아아아아아아아아아"}/>
+                <MessengerReceive ReceiveText={"test입니다. 아아아아아아아아아"}/>
+                <MessengerSend SendText={"test입니다. 아아아아아아아아아"}/>
+                <MessengerSend SendText={"test입니다. 아아아아아아아아아"}/>
+                <MessengerSend SendText={"test입니다. 아아아아아아아아아"}/>
+                <MessengerReceive ReceiveText={"test입니다. 아아아아아아아아아"}/>
+                <MessengerSend SendText={"afjaaaaaaaafefsafdsafdsafdsafdsafdsafdsafdsafdsafdsaffdsafdsa"}/>
+                <MessengerReceive ReceiveText={"test입니다. 아아아아아아아아아"}/>
+        </MessengerScroll>
+    );
+  }
+  
+  export default MessengerChatWrap;
