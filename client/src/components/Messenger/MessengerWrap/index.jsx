20aa23322e9053eb36047a6890af6335235d9264
deokisys <sjb378@me.com>
2019-11-14 17:21:57 +0900

    Add messenger room element and show
    메신저 방의 목록과 각 방의 컴포넌트를 구현
    메신저 방은 상대방의 프로필, 아이디, 최신 메시지를 표시
    최신 메시지는 일정 길이 이상은 생략표시

--- client/src/components/Messenger/MessengerWrap/index.jsx
+++ client/src/components/Messenger/MessengerWrap/index.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import React, { useState , useEffect} from 'react';
-
+import MessengerRoom from './MessengerRoom';
 const MessengerDiv= styled.div` 
     position:fixed;
     bottom:7rem;
@@ -9,6 +9,8 @@ const MessengerDiv= styled.div`
     height:25rem;
     border:solid 0.1rem;
     border-color:#FEAA6E;
+    background-color:white;
+
     &::after{
         content: '';
         position: absolute;
@@ -20,11 +22,27 @@ const MessengerDiv= styled.div`
     } 
 `;
 
+const MessengerScroll = styled.div`
+width:100%;
+height:100%;
+overflow-x:hidden;
+overflow-y:auto;
+`;
 function MessengerWrap(props) {
 
     return (
         <MessengerDiv>
-
+            <MessengerScroll>
+                <MessengerRoom Img={"A"} Name={"과장님"} RecentMsg={"ㅇ야ㅑ야야야야ㅑ야야야야ㅑ야야야ㅑ야야야야ㅑ양"}/>
+                <MessengerRoom Img={"B"} Name={"주임님"} RecentMsg={"부재중 전화 11통"}/>
+                <MessengerRoom Img={"E"} Name={"거래처"} RecentMsg={"부재중 전화 2통"}/>
+                <MessengerRoom Img={"A"} Name={"과장님"} RecentMsg={"부재중 전화 21통"}/>
+                <MessengerRoom Img={"B"} Name={"주임님"} RecentMsg={"부재중 전화 11통"}/>
+                <MessengerRoom Img={"E"} Name={"거래처"} RecentMsg={"부재중 전화 2통"}/>
+                <MessengerRoom Img={"A"} Name={"과장님"} RecentMsg={"부재중 전화 21통"}/>
+                <MessengerRoom Img={"B"} Name={"주임님"} RecentMsg={"부재중 전화 11통"}/>
+                <MessengerRoom Img={"E"} Name={"거래처"} RecentMsg={"부재중 전화 2통"}/>
+            </MessengerScroll>
         </MessengerDiv>
     );
   }
