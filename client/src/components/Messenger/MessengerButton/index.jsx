0bbcd2b4ba785b817af27db7ab47111938d68bdb
deokisys <sjb378@me.com>
2019-11-15 13:44:01 +0900

    Review week 2
    2주차 리뷰파일입니다.
    - 메신저저ui, 거래내역 ui

---
+++ client/src/components/Messenger/MessengerButton/index.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import React, { useState , useEffect} from 'react';
+
+const ShowButton= styled.button` 
+    all:unset;
+    position:fixed;
+    bottom:1rem;
+    right:1rem;
+
+    background-color:#FEAA6E;
+    color:white;
+
+    border-radius:4rem;
+    border-width: 3px;
+    border-style: solid;
+    border-color: #FEAA6E;;
+
+    font-weight:bold;
+    text-align:center;
+    width:4rem;
+    height:4rem;
+   
+    &:hover{
+        cursor:pointer;
+    }
+`;
+
+function MessengerButton(props) {
+
+    return (
+      <>
+        <ShowButton onClick={props.select}>메신저</ShowButton>
+      </>
+    );
+  }
+  
+  export default MessengerButton;
