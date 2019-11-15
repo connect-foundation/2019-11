9ceea1a515d7e179c14bb5630d3b6eab88a64769
seongchanchoi <chsch911028@gmail.com>
2019-11-15 14:57:51 +0900

    Fix merge conflict

--- client/src/components/CategoryBar/CategoryIcon/index.jsx
+++ client/src/components/CategoryBar/CategoryIcon/index.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import styled from 'styled-components'
-import ImgIcon from './ImageIcon'
-import TextIcon from './TextIcon'
+import React from "react";
+import styled from "styled-components";
+import ImgIcon from "./ImageIcon";
+import TextIcon from "./TextIcon";
 
 const Wrapper = styled.li`
-    display: flex;
-    position: relative;
-    width: 100%;
-    padding-top: 100%;
-    margin-bottom: 0.5em;
-    background: ${props => props.color};
-    border-radius: 20%;
-`
+  display: flex;
+  position: relative;
+  width: 100%;
+  padding-top: 100%;
+  margin-bottom: 0.5em;
+  background: ${props => props.color};
+  border-radius: 20%;
+`;
 
-const Components = (props) => {
-    
-    return (
-        <Wrapper color={props.color}>
-            <ImgIcon img={props.img}></ImgIcon>
-            <TextIcon color={props.color} active={props.active} onClick={props.onClick}>{props.text}</TextIcon>
-        </Wrapper>
-    );
-}
+const Components = props => {
+  return (
+    <Wrapper color={props.color}>
+      <ImgIcon img={props.img}></ImgIcon>
+      <TextIcon
+        color={props.color}
+        active={props.active}
+        onClick={props.onClick}
+      >
+        {props.text}
+      </TextIcon>
+    </Wrapper>
+  );
+};
 
-export default Components
+export default Components;
