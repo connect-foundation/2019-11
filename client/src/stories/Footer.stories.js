9282007b99a826a68878924b7e6b8c1dde9e17bc
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-17 16:29:08 +0900

    Update stories
    컴포넌트 확인을 위한 스토리 추가
    Atomic Desgin으로 변경 시 전부 바뀔 예정

---
+++ client/src/stories/Footer.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Footer from '../components/Footer'
+
+export default {
+    title: 'Footer',
+};
+
+export const footer = () => {
+    return (
+        <Footer/>
+    )
+}
