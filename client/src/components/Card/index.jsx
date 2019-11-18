0e195fab75347e6800f1915d4eb20840728a0bb4
sphong5911 <sphong5911@naver.com>
2019-11-18 16:48:22 +0900

    Add CardStyles for Card component
    정의가 길어지는 styled component 의 코드를 별도로 분리

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 import Thumbnail from './Thumbnail';
 import TagContainer from './TagContainer';
-import CardTitle from './CardTitle';
 import Bids from './Bids';
 import PriceContainer from './PriceContainer'
-
-const CardStyle = styled.div`
-  border-radius: 1rem;
-  margin: 1rem;
-  background: white;
-  width: 13rem;
-  height: 17rem;
-  padding: 0;
-  cursor: pointer;
-  box-shadow: 0 0.1rem 0.4rem 0 rgba(0, 0, 0, 0.2), 0 0.3rem 0.2rem 0 rgba(0, 0, 0, 0.19);
-  transition: all .15s ease-in-out;
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-`;
+import { CardStyle, CardTitle, InfoContainer } from './CardStyles';
 
 const Card = ({item}) => {
   const { thumbnail,
