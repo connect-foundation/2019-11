c90aa395fc5c8adf77d1b59a629c38b7048c1f05
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-18 09:59:31 +0900

    Update box link move to complete template
    Box link의 재사용성이 낮아 template로 이동
    너무 작은 컴포넌트 제거

--- client/src/pages/Regiester/template/Complete/index.jsx
+++ client/src/pages/Regiester/template/Complete/index.jsx
@@ -18,6 +18,26 @@ const ButtonDiv = styled.div`
     margin: 0 auto;
 `
 
+const LinkBox = styled.a`
+    width: 100%;
+    height: 4rem;
+    padding: 0.5rem 0.25rem;
+    border: #bfbfbf solid 1px;
+    background: #eeeeee;
+    font-size: 1.3rem;
+    box-sizing:border-box;
+    text-decoration:none;
+    color: black;
+    line-height:2rem;
+
+    transition: border .2s ease-in-out;
+
+    &:hover, &:focus{
+        border: #ffae6a solid 2px;
+        border-radius: 10px;
+    }
+`
+
 const Component = (props) => {
     const { width, next } = props
 
@@ -25,9 +45,9 @@ const Component = (props) => {
         <PageBase width={width}>
             <ContentDiv>
                 <ButtonDiv>
-                    <BoxLink href="/register">새 상품등록</BoxLink>
-                    <BoxLink href="/">마이페이지로</BoxLink>
-                    <BoxLink href="/">확인</BoxLink>
+                    <LinkBox href="/register">새 상품등록</LinkBox>
+                    <LinkBox href="/">마이페이지로</LinkBox>
+                    <LinkBox href="/">확인</LinkBox>
                 </ButtonDiv>
             </ContentDiv>
         </PageBase>
