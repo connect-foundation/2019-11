304767db5922d63c7b7efbfb44b95e71a60d24f7
deokisys <sjb378@me.com>
2019-11-19 15:57:25 +0900

    Remove example file
    예시로 작성한 파일 삭제 및 주석 처리

--- server/src/database/factories/ReservationFactory.ts
+++ server/src/database/factories/ReservationFactory.ts
@@ -1,9 +1,9 @@
-import * as Faker from 'faker';
-import { define } from 'typeorm-seeding';
-import { Reservations } from '../../models/Reservations';
+// import * as Faker from 'faker';
+// import { define } from 'typeorm-seeding';
+// import { Reservations } from '../../models/Reservations';
 
-define(Reservations, (faker: typeof Faker, settings: any): Reservations => {
-  const reservation = new Reservations();
+// define(Reservations, (faker: typeof Faker, settings: any): Reservations => {
+//   const reservation = new Reservations();
 
-  return reservation;
-});
+//   return reservation;
+// });
--- server/src/database/factories/UserFactory.ts
+++ server/src/database/factories/UserFactory.ts
@@ -1,15 +1,15 @@
-import { Users } from '../../models/Users';
-import * as Faker from 'faker';
-import { define } from 'typeorm-seeding';
+// import { Users } from '../../models/Users';
+// import * as Faker from 'faker';
+// import { define } from 'typeorm-seeding';
 
-define(Users, (faker: typeof Faker, settings: any): Users => {
-  const user = new Users();
-  faker.locale = 'ko';
-  user.name = `${faker.name.lastName()}${faker.name.firstName()}`;
-  faker.locale = 'en';
-  user.loginId = faker.lorem.word();
-  user.password = faker.random.number().toString();
-  user.super = !!(faker.random.number() % 2);
+// define(Users, (faker: typeof Faker, settings: any): Users => {
+//   const user = new Users();
+//   faker.locale = 'ko';
+//   user.name = `${faker.name.lastName()}${faker.name.firstName()}`;
+//   faker.locale = 'en';
+//   user.loginId = faker.lorem.word();
+//   user.password = faker.random.number().toString();
+//   user.super = !!(faker.random.number() % 2);
 
-  return user;
-});
+//   return user;
+// });
