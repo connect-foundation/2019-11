50380651710d055b8f423d617b671327b8a70ab8
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-19 18:31:52 +0900

    Update font unit em to rem
    폰트 단위를 em에서 rem으로 변경
    팀 개발 규칙 적용

--- client/src/components/ListBox/ListHeader/index.jsx
+++ client/src/components/ListBox/ListHeader/index.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     width: 100%;
-    height: 2.5em;
+    height: 2.5rem;
     box-sizing: border-box;
-    padding: 0.15em;
+    padding: 0.15rem 0.5rem;
     background-color:#ffa66a;
     color:white;
     display:flex;
@@ -15,10 +15,10 @@ const Container = styled.div`
 const Title = styled.span`
     width: 100%;
     height:fit-content;
-    font-size: 1.3em;
+    font-size: 1.3rem;
     font-weight: 400;
     margin: auto;
-    letter-spacing: 0.5em;
+    letter-spacing: 0.5rem;
 `
 
 const Components = props => {
