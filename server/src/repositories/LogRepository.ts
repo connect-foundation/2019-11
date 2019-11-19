2c6118574f3937ed7599c290db5f6b8c6bd6ed36
deokisys <sjb378@me.com>
2019-11-20 00:42:01 +0900

    Add tradelist api for get users buylogs
    거래내역 api중 유저가 구매한 내역만 출력하는 api를 구현
    사용법 POST /api/log/filter body:{"userid":1,"buy":true}

---
+++ server/src/repositories/LogRepository.ts
@@ -0,0 +1,29 @@
+import { EntityRepository, EntityManager } from 'typeorm';
+import { Auction_logs } from '../models/Auction_logs';
+
+@EntityRepository()
+export class LogReporsitory {
+  constructor(private readonly em: EntityManager) {}
+
+  public find() {
+    return this.em.find(Auction_logs);
+  }
+
+  public findOne(id: number) {
+    return this.em.findOne(Auction_logs, id);
+  }
+
+  public findBuyLogs(user_id : number) {
+    return this.em.find(Auction_logs,{
+        where:{
+            user: { id: user_id },
+            is_winning:true
+        }
+    });
+  }
+
+
+  public save(Auction_logs: Auction_logs) {
+    return this.em.save(Auction_logs);
+  }
+}
