6ee5109222824585b7d2fcfe89c0ad80a09ede76
sphong5911 <sphong5911@naver.com>
2019-11-19 18:34:53 +0900

    Update ExpandList component
    카테고리별 항목을 보여주기 위해 DetailCategory 추가

--- client/src/components/CategoryBar/ExpandList/index.jsx
+++ client/src/components/CategoryBar/ExpandList/index.jsx
@@ -3,19 +3,39 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     display: flex;
+    flex-direction: column;
     width: 15em;
     height: 100%;
     overflow-y: auto;
-    background-color: #ffe1a2;
-    border-right: #FFA626 solid 2px;
+    background-color: #ffe1a2;  
     box-sizing: border-box;
     z-index: 1;
 `;
 
+const DetailCategory = styled.div`
+    font-family: "BMJUA";
+    font-size: large;
+    text-align: center;
+    padding: 1em;
 
-const Components = (props) => {
+    transition: all .3s ease-in-out;
+    &:hover {
+        background-color: #EC8852;
+    }
+`;
+
+
+const Components = ({idx, open, details}) => {
+    const detailCategoryList = details.details;
+
     return (
-        <Container open={props.open}/>
+        <Container idx={idx} open={open}>
+            {detailCategoryList.map(
+                category => 
+                <DetailCategory>
+                    <label>{category.title}</label>
+                </DetailCategory>)}
+        </Container>
     )
 }
 
