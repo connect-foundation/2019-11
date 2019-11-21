28b507cc0cd1622d3a1fc1c0afa56aab3fcb0a32
seongchanchoi <chsch911028@gmail.com>
2019-11-21 13:31:56 +0900

    Add Dockerfile for chat-server development environment
    [why]
     - chat-server 개발환경 구성을 위해서
    [what]
     - dockerfile 생성 및 docker-compose 파일과 연결

--- chat-server/index.js
+++ chat-server/index.js
@@ -2,18 +2,23 @@ const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-server.listen(4000);
+server.listen(4000, () => {
+  console.log("##### CHAR-SERVER STARTED #####");
+});
 // WARNING: app.listen(4000) will NOT work here!
 
 const rooms = {};
 
 io.on("connection", function(socket) {
   socket.on("message", ({ roomId, sessionId, userId, text }) => {
+    console.log(
+      `##### USER(${sessionId}, ${userId})가 ${roomId}방에 ${text} 문자 전송 #####`
+    );
     io.to(roomId).emit("message", { sessionId, userId, text });
   });
 
   socket.on("joinRoom", ({ roomId, sessionId, userId }) => {
-    console.log(roomId, sessionId, userId);
+    console.log(`##### USER(sessionId, userId)가 ${roomId}방에 접속 #####`);
     //해당 socket(client)를 특정 room에 Join 시킨다.
     socket.join(roomId, () => {
       // 특정 room에 입장해있는 user를 전역객체에 저장한다.
@@ -28,6 +33,7 @@ io.on("connection", function(socket) {
   });
 
   socket.on("leaveRoom", (roomId, sessionId, userId) => {
+    console.log(`##### USER(sessionId, userId)가 ${roomId}방에서 나감 #####`);
     socket.leave(roomId, () => {
       //rooms 정보에서 삭제한다.
       delete rooms[roomId][sessionId];
