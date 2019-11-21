88ac4f4a07edcc9c2af40c08f1222706a1014965
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-21 22:09:23 +0900

    Update storybook
    많이 바뀌어야 하는 스토리는 일단 삭제
    차후 디자인에 따라 추가 예정
    일부 파일 경로 변경 반영

--- client/src/stories/5-Carousel.stories.js
+++ client/src/stories/5-Carousel.stories.js
@@ -1,35 +1,10 @@
 import React from 'react'
 
-import AddButton from '../components/Carousel/AddButton'
-import RemoveButton from '../components/Carousel/RemoveButton'
-import CarouselItem from '../components/Carousel/CarouselImage'
-import Carousel from '../components/Carousel'
+import Carousel from '../components/Molecules/Carousel'
 
 export default {
     title: 'Carousel',
 };
-
-export const removeButton = () => {
-    return(
-        <div style={{background:'#d0d0d0'}}>
-        <RemoveButton/>
-        </div>
-    )
-}
-
-export const carouselImage = () => {
-    return (
-        <div style={{width:500, hegith:500}}>
-            <CarouselItem src="http://miro.medium.com/max/318/1*1OKmA2EdGln8O6RCVORgGg.png"/>
-        </div>
-    )
-}
-
-export const carouselImageAdd = () => {
-    return (
-        <AddButton/>
-    )
-}
 
 export const carousel = () => {
     return (
