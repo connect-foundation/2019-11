818ed8e07b442a2a566586c98e11aac242397b36
deokisys <sjb378@me.com>
2019-11-25 16:03:17 +0900

    Add if click background close messenger
    배경을 클릭시 메신저가 닫히도록 수정
    메신저 이외의 영역을 클릭시 닫힘

--- client/src/components/Messenger/index.jsx
+++ client/src/components/Messenger/index.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import MainButton from './MainButton';
 import Container from './Container';
+import styled from 'styled-components';
+
+const MessengerBack = styled.div`
+  display:block;
+  position:absolute;
+  width:100%;
+  height:100%;
+`;
 
 function Messenger(props) {
     const [show,setShow] = useState(false);
@@ -11,7 +19,12 @@ function Messenger(props) {
 
     let MessengerContent = null;
     if(show){
-      MessengerContent = <Container/>
+      MessengerContent = 
+      <>
+        <MessengerBack onClick={()=>ChangeState()}>
+        </MessengerBack>
+        <Container/>
+      </>
     }
     return (
       <>
