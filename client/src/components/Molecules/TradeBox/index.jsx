dca15d95721730aa372b86adbe84441b60bd3ca4
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-25 21:59:53 +0900

    Update tradebox
    UX 개선
    거래 기록에 hover시 이미지 확대 애니메이션 추가
    날짜 영역 추가

--- client/src/components/Molecules/TradeBox/index.jsx
+++ client/src/components/Molecules/TradeBox/index.jsx
@@ -1,54 +1,72 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 
 const Container = styled.div`
-    display:flex;
-    width: 100%;
-    max-width: fit-content;
-    height: 5rem;
-    margin-bottom: 5px;
+  display: flex;
+  width: 100%;
+  min-height: fit-content;
+  margin-bottom: 5px;
+  justify-content: space-between;
+
+  &:hover {
+    img {
+      width: 7rem;
+      height: 7rem;
+    }
+  }
 `
 
 const Thumbnail = styled.img`
-    width: 5rem;
-    height: 5rem;
+  width: 5rem;
+  height: 5rem;
+
+  transition: width 0.15s ease-in-out, height 0.15s ease-in-out;
 `
 
 const Title = styled.span`
-    display:flex;
-    width: 25rem;
-    height: fit-content;
-    font-size: 1.5rem;
-    margin: auto 0.5rem;
+  display: flex;
+  width: 20rem;
+  height: fit-content;
+  font-size: 1.3rem;
+  margin: auto 1rem;
 `
 
 const Status = styled.span`
-    min-width: 7rem;
-    height: fit-content;
-    font-size: 1.2rem;
-    margin: auto 0.5rem;
-    text-align: center;
-    font-weight: 700;
+  min-width: 7rem;
+  height: fit-content;
+  font-size: 1.1rem;
+  margin: auto 0.5rem;
+  text-align: center;
+  font-weight: 700;
 `
 
 const Price = styled.span`
-    min-width: 10rem;
-    height: fit-content;
-    font-size: 1.2rem;
-    margin: auto 0.5rem;
-    text-align:end;
+  min-width: 10rem;
+  height: fit-content;
+  font-size: 1rem;
+  margin: auto 0.5rem;
+  text-align: end;
+`
+
+const Date = styled.span`
+  min-width: 5rem;
+  height: fit-content;
+  margin: auto 0.5rem;
+  text-align: end;
+  font-weight: 700;
+  color: #dfdfdf;
 `
 
 const Component = ({ title, thumbnail, status, price }) => {
-
-    return (
-        <Container>
-            <Thumbnail src={thumbnail} />
-            <Title>{title}</Title>
-            <Status>{status}</Status>
-            <Price>{price}원</Price>
-        </Container>
-    )
+  return (
+    <Container>
+      <Thumbnail src={thumbnail} />
+      <Title>{title}</Title>
+      <Status>{status}</Status>
+      <Price>{price}원</Price>
+      <Date>3일전</Date>
+    </Container>
+  )
 }
 
 export default Component
