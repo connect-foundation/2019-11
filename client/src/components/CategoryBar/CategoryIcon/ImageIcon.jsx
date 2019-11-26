1a8af6ea58df0244dbc72a133c04b46f50282eb4
sphong5911 <sphong5911@naver.com>
2019-11-26 14:18:47 +0900

    Update CategoryBar component codes
    Prettier를 이용하여 코드 스타일 수정

--- client/src/components/CategoryBar/CategoryIcon/ImageIcon.jsx
+++ client/src/components/CategoryBar/CategoryIcon/ImageIcon.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const ImgIconStyle = styled.div`
-    position: absolute;
-    display: flex;
+  position: absolute;
+  display: flex;
 
-    top:0;
-    left:0;
-    bottom:0;
-    right:0;
-    z-index: 0;
-    justify-content: center;
-    align-items: center;
-    background: ${props => props.color};
-    img {
-        width: 70%;
-        height: 70%;
-    }
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 0;
+  justify-content: center;
+  align-items: center;
+  background: ${props => props.color};
+  img {
+    width: 70%;
+    height: 70%;
+  }
 `;
 
-const ImgIcon = ({color, img}) => {
-    return (
-        <ImgIconStyle color={color}>
-            <img src={img}/>
-        </ImgIconStyle>
-    );
-}
+const ImgIcon = ({ color, img }) => {
+  return (
+    <ImgIconStyle color={color}>
+      <img src={img} />
+    </ImgIconStyle>
+  );
+};
 
-export default ImgIcon
+export default ImgIcon;
--- client/src/components/CategoryBar/ExpandList/index.jsx
+++ client/src/components/CategoryBar/ExpandList/index.jsx
@@ -1,47 +1,46 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 15em;
-    height: 100%;
-    overflow-y: auto;
-    background-color: #ffe1a2;  
-    box-sizing: border-box;
-    z-index: 1;
+  display: flex;
+  flex-direction: column;
+  width: 15em;
+  height: 100%;
+  overflow-y: auto;
+  background-color: #ffe1a2;
+  box-sizing: border-box;
+  z-index: 1;
 `;
 
 const DetailCategory = styled.div`
-    font-family: "BMJUA";
-    font-size: large;
-    text-align: center;
-    padding: 1em;
+  font-family: "BMJUA";
+  font-size: large;
+  text-align: center;
+  padding: 1em;
+  cursor: pointer;
+
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    background-color: #ec8852;
+  }
+
+  label {
     cursor: pointer;
-
-    transition: all .3s ease-in-out;
-    &:hover {
-        background-color: #EC8852;
-    }
-
-    label {
-        cursor: pointer;
-    }
+  }
 `;
 
+const Components = ({ idx, open, details }) => {
+  const detailCategoryList = details.details;
 
-const Components = ({idx, open, details}) => {
-    const detailCategoryList = details.details;
+  return (
+    <Container idx={idx} open={open}>
+      {detailCategoryList.map(category => (
+        <DetailCategory>
+          <label>{category.title}</label>
+        </DetailCategory>
+      ))}
+    </Container>
+  );
+};
 
-    return (
-        <Container idx={idx} open={open}>
-            {detailCategoryList.map(
-                category => 
-                <DetailCategory>
-                    <label>{category.title}</label>
-                </DetailCategory>)}
-        </Container>
-    )
-}
-
-export default Components
+export default Components;
