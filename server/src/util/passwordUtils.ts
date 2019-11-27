81e1283b9dbf6351a778ac46c21ae9944ed51a14
sphong5911 <sphong5911@naver.com>
2019-11-27 17:49:00 +0900

    Add passwordUtils
    회원가입 시 사용자로부터 입력받은 password를 암호화하는 함수 추가
    로그인 시 사용자로부터 입력받은 password를 암호화한 것과 DB에 저장된 password를
    비교하는 함수 추가

--- server/src/util/passwordUtils.ts
+++ server/src/util/passwordUtils.ts
@@ -1,3 +1,16 @@
 import crypto from "crypto";
 
-export function encryptPassword(password: string) {}
+export function encryptPassword(password: string) {
+  const salt = crypto.randomBytes(30).toString("base64");
+  const key = crypto.pbkdf2Sync(password, salt, 102935, 30, "sha512");
+  return { salt, result: key.toString("base64") };
+}
+
+export function checkPassword(
+  inputPwd: string,
+  encryptedPwd: string,
+  salt: string
+) {
+  const key = crypto.pbkdf2Sync(inputPwd, salt, 102935, 30, "sha512");
+  return key.toString("base64") === encryptedPwd;
+}
