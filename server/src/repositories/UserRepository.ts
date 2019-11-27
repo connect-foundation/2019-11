151f931e8a459a331f44869ce4c19883140aa7dd
seongchanchoi <chsch911028@gmail.com>
2019-11-27 10:58:31 +0900

    Refactor api-server entire directory
    [why]
     - api-server 재구성을 위해서
    [what]
     - 불필요한 파일 삭제

--- server/src/repositories/UserRepository.ts
+++ server/src/repositories/UserRepository.ts
@@ -1,5 +1,5 @@
-import { EntityRepository, Repository, EntityManager } from 'typeorm';
-import { Users } from '../models/Users';
+import { EntityRepository, EntityManager } from "typeorm";
+import { Users } from "../models/Users";
 /** Entity Manager - Constructor Injecction
  *  1. TypeOrm 역시 TypeDI의 Container를 사용한다.(server.ts 참조)
  *  2. 그래서 TypeOrm의 EntityManager는 TypeDI에 등록되있다.
