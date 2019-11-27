e85e116d040440b0fe4cde226b8c179668548d76
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-28 00:16:41 +0900

    Update fetchUtil
    비구조할당이 가능한 형태로 변경
    불필요한 이름 중복 삭제

--- client/src/utils/fetchUtil.js
+++ client/src/utils/fetchUtil.js
@@ -1,26 +1,27 @@
-export default {
-    fetchAsync(url, option, callback) {
-        fetch(url, option)
-            .then((res) => {
-                return res.json();
-            }).then((json) => {
-                if (callback !== undefined)
-                    callback(json)
-            })
-    },
-    async fetchAwait(url, option) {
-        const res = await fetch(url, option)
-            .then((res) => {
-                return res.json();
-            }).then((json) => {
-                return json;
-            })
-
-        return res;
-    },
-    option: {
-        get: { method: 'GET', headers: { 'User-Agent': 'Mozilla/5.0' } },
-        post: { method: 'POST', headers: { 'User-Agent': 'Mozilla/5.0' } },
-        post_json: { method: 'POST', headers: { 'User-Agent': 'Mozilla/5.0', 'Content-Type': 'application/json' } }
-    }
+export const Async = (url, option, callback) => {
+    fetch(url, option)
+        .then((res) => {
+            return res.json();
+        }).then((json) => {
+            if (callback !== undefined)
+                callback(json)
+        })
 }
+
+export const Await = async (url, option) => {
+    return await fetch(url, option)
+        .then((res) => {
+            return res.json();
+        }).then((json) => {
+            return json;
+        })
+}
+
+export const Option = {
+    get: { method: 'GET', headers: { 'User-Agent': 'Mozilla/5.0' } },
+    post: { method: 'POST', headers: { 'User-Agent': 'Mozilla/5.0' } },
+    postJson: { method: 'POST', headers: { 'User-Agent': 'Mozilla/5.0', 'Content-Type': 'application/json' } }
+}
+
+
+export default { Async, Await, Option }
