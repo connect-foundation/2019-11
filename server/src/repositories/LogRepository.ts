933c6c969f41b4ba44e62c0f0ca1e0f39fb45b58
deokisys <sjb378@me.com>
2019-11-27 15:00:15 +0900

    Fix tradelist get buy logs to typeorm
    typeorm을 활용하여 재설계된 데이터베이스에 맞게 typeorm의 find로
    구매 내역을 출력

---
+++ server/src/repositories/LogRepository.ts
@@ -0,0 +1,32 @@
+import { EntityRepository, EntityManager, MoreThanOrEqual } from "typeorm"
+import { prevDay } from "../util/DateUtils"
+import { Products } from "../models/Products"
+import { Users } from "../models/Users"
+
+@EntityRepository()
+export class LogRepository {
+  constructor(private readonly em: EntityManager) {}
+
+  public find() {
+    return this.em.find(Products)
+  }
+  public findBuy(userid: number, dayago: number, page: number, limit: number) {
+    return this.em.findAndCount(Products, {
+      relations: ["seller"],
+      where: {
+        soldDate: MoreThanOrEqual(prevDay(dayago)),
+        buyerId: userid
+      },
+      order: {
+        soldDate: "DESC"
+      },
+      skip: (page - 1) * limit,
+      take: limit,
+      cache: true
+    })
+  }
+
+  public findOne(id: number) {
+    return this.em.findOne(Products, id)
+  }
+}
---
+++ server/src/services/LogService.ts
@@ -0,0 +1,33 @@
+import { Service } from "typedi"
+import { LogRepository } from "../repositories/LogRepository"
+import { InjectRepository } from "typeorm-typedi-extensions"
+import { Products } from "../models/Products"
+
+/** TODO: Transaction을 어떻게 처리해야 좋을까? */
+@Service()
+export class LogService {
+  constructor(@InjectRepository() private readonly LogRepository: LogRepository) {}
+
+  /** GET */
+  public find() {
+    return this.LogRepository.find()
+  }
+
+  public findOne(id: number) {
+    return this.LogRepository.findOne(id)
+  }
+
+  public findBuyLog(userid: number, dayago: number, page: number, limit: number) {
+    return this.LogRepository.findBuy(userid, dayago, page, limit)
+  }
+
+  /** PUT, PATCH */
+  public update(id: number, product: Products) {
+    /**TODO: 해당 id값으로 Entitiy를 조회해서, 새로운 user 엔티티로 변경 */
+  }
+
+  /** DELETE */
+  public delete(id: number) {
+    /**TODO: 해당 id값으로 Enitity 삭제 */
+  }
+}
