4355b7d2b8463cf549d8530853573e7107dcb3c1
Girlfirend-Yerin <hsunj2003@naver.com>
2019-11-28 15:05:19 +0900

    Update fetch util n service
    put으로 보내기 위한 옵션 추가
    옵션 추가에 따른 서비스 추가

--- client/src/services/fetchService.js
+++ client/src/services/fetchService.js
@@ -9,4 +9,13 @@ export const jsonFetch = async (url, headerOption, body) => {
     return result
 }
 
-export default { jsonFetch }
+export const putJsonFetch = async (url, headerOption, body) => {
+    const option = Option.putJsonFetch
+    option.body = JSON.stringify(body)
+    option.headers = Object.assign(option.headers, headerOption)
+    const result = await Await(url, option)
+
+    return result
+}
+
+export default { jsonFetch, putJsonFetch }
