872e9233d3c9857b277c90a7b317593267a145ef
Girlfirend-Yerin <hsunj2003@naver.com>
2019-12-02 13:32:58 +0900

    Update validator utils
    해당 값이 숫자인지 판별하는 validator 추가
    숫자 입력창에 문자가 들어오는걸 방지하기위해 추가

--- client/src/utils/validator.js
+++ client/src/utils/validator.js
@@ -6,8 +6,12 @@ export const isArrayEmpty = (list) => !list.length
 
 export const strLengthCheck = (str, maxLen) => str.length > maxLen
 
+export const isNumber = (key) => ('0' <= key && key <= '9')
+
 export default {
     strEmpty,
     idxNotSelected,
-    isArrayEmpty
+    isArrayEmpty,
+    strLengthCheck,
+    isNumber
 }
