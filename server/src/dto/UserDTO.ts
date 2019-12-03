22064e733fd493348faff954fbf4c3a014b59b07
sphong5911 <sphong5911@naver.com>
2019-12-03 17:20:39 +0900

    Add UserDTO for response user info
    민감한 정보를 제외한 사용자 정보를 반환하기 위하여 UserDTO 추가

---
+++ server/src/dto/UserDTO.ts
@@ -0,0 +1,10 @@
+export class UserDTO {
+  id: number;
+  loginId: string;
+  name: string;
+  profileUrl: string;
+  mannerPoint: number;
+  email: string;
+  accessToken: string;
+  refreshToken: string;
+}
