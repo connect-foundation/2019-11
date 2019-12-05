809ae661285708119e148741170d0b81e705828d
Girlfirend-Yerin <hsunj2003@naver.com>
2019-12-05 16:57:30 +0900

    Update fetchService
    get을 제공하는 fetch추가
    body 대신 params를 받는다.

--- client/src/components/Organisim/TradeListBox/index.jsx
+++ client/src/components/Organisim/TradeListBox/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import styled from "styled-components"
-import TradeBox from "../TradeBox"
+import TradeBox from "../TradeBase"
 const TradeContents = styled.div`
   display: ${props => (props.isHover ? "flex" : "none")};
   flex-direction: row;
--- client/src/services/fetchService.js
+++ client/src/services/fetchService.js
@@ -1,6 +1,15 @@
 import { Await, Option } from '../utils/fetchUtil.js'
 
-export const jsonFetch = async (url, headerOption, body) => {
+export const getFetch = async (url, headerOption, params) => {
+    const option = Option.get;
+    option.params = params
+    option.headers = Object.assign(option.headers, headerOption)
+    const result = await Await(url, option)
+
+    return result
+}
+
+export const postJsonFetch = async (url, headerOption, body) => {
     const option = Option.postJson
     option.body = JSON.stringify(body)
     option.headers = Object.assign(option.headers, headerOption)
@@ -18,4 +27,4 @@ export const putJsonFetch = async (url, headerOption, body) => {
     return result
 }
 
-export default { jsonFetch, putJsonFetch }
+export default { getFetch, postJsonFetch, putJsonFetch }
