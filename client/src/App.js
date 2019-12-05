df1b8056b5e402362b0e8c03831d6876b34d84e1
SeongChan Choi <chsch911028@gmail.com>
2019-12-06 01:03:42 +0900

    Implement modal
    [what]
     - Modal 컴포넌트 구현
     - 하위 컴포넌트에서 사용할 수 있도록 Modal 컴포넌트 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,42 @@
-import React, { useState, useEffect } from "react"
-import { BrowserRouter } from "react-router-dom"
-import Router from "./Router"
-import UserContext from "./context/UserContext"
-import "./style/App.css"
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
+import UserContext from "./context/UserContext";
+import "./style/App.css";
+import ModalContext from "./context/ModalContext";
 
 function App() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
+  const [modal, setModal] = useState({
+    isOpen: false,
+    component: null,
+    message: ""
+  });
+  const ModalContent = modal.component;
+
+  const handleClickModalBack = e => {
+    setModal(state => ({ ...state, isOpen: false }));
+  };
 
   return (
     <BrowserRouter>
-      <UserContext.Provider value={[user, setUser]}>
-        <div className="App">
-          <Router />
-        </div>
-      </UserContext.Provider>
+      <ModalContext.Provider value={[modal, setModal]}>
+        {modal.isOpen ? (
+          <div id="modal">
+            <div className="background" onClick={handleClickModalBack}></div>
+            <div className="content">
+              <ModalContent message={modal.message} />
+            </div>
+          </div>
+        ) : null}
+        <UserContext.Provider value={[user, setUser]}>
+          <div className="App">
+            <Router />
+          </div>
+        </UserContext.Provider>
+      </ModalContext.Provider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
