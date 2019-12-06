d44d4928f4947623266ceb4c319ea3ce5047aeca
deokisys <sjb378@me.com>
2019-12-07 03:35:46 +0900

    Fix storybook path
    변경된 경로에 맞게 수정

--- client/src/stories/Organisms/Messenger.stories.js
+++ client/src/stories/Organisms/Messenger.stories.js
@@ -9,7 +9,9 @@ export default {
 }
 
 //방만들기 버튼
-export const MessengerCreateButton = () => <CreateButton userId={"a"} sellerId={"b"}></CreateButton>
+export const MessengerCreateButton = () => (
+  <CreateButton userId={"104731762607210695795"} sellerId={"a"}></CreateButton>
+)
 
 //메시지 목록 요소
 
