d05204af6c6fce798736cc39b248267c8fb9372f
sphong5911 <sphong5911@naver.com>
2019-12-06 16:03:22 +0900

    Add review codes

---
+++ server/src/controllers/api/LoginController.ts
@@ -0,0 +1,134 @@
+import {
+  JsonController,
+  Post,
+  BodyParam,
+  Req,
+  Res,
+  Get
+} from "routing-controllers";
+import { UserService } from "../../services/UserService";
+import { Users } from "../../models/Users";
+import jwt from "jsonwebtoken";
+import uuid from "uuid";
+import fetch from "node-fetch";
+import { google } from "../../constants/oauthAPIs";
+import { Await, Option } from "../../util/fetchUtil";
+
+@JsonController("/sign")
+export class LoginController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post("/login")
+  public async login(
+    @BodyParam("username") loginId: string,
+    @BodyParam("password") password: string,
+    @Req() req: any
+  ) {
+    const { msg } = await this.userService.checkLogin(loginId, password);
+    if (msg) {
+      const accessToken = await jwt.sign(
+        { loginId },
+        `${process.env.JWT_KEY}`,
+        { expiresIn: "2h" }
+      );
+
+      const refreshToken = await jwt.sign(
+        { loginId },
+        `${process.env.JWT_KEY}`,
+        { expiresIn: "3 days" }
+      );
+      const user = await this.userService.updateToken(
+        loginId,
+        accessToken,
+        refreshToken
+      );
+      return { msg, user };
+    }
+    return { msg, user: null };
+  }
+
+  @Post("/kakao")
+  public async authKakao(
+    @BodyParam("id") loginId: string,
+    @BodyParam("name") name: string,
+    @BodyParam("email") email: string,
+    @BodyParam("profileUrl") profileUrl: string,
+    @Req() req: any
+  ) {
+    const accessToken = `kakao_${req.headers["access-token"]}`;
+    const refreshToken = `kakao_${req.headers["refresh-token"]}`;
+
+    if (await this.userService.checkDuplicate(loginId)) {
+      const user = await this.userService.updateAuth(
+        loginId,
+        name,
+        email,
+        profileUrl,
+        accessToken,
+        refreshToken
+      );
+      return { msg: true, user };
+    }
+    const password = uuid();
+    const user = await this.userService.createAuth(
+      loginId,
+      password,
+      name,
+      email,
+      profileUrl,
+      accessToken,
+      refreshToken
+    );
+
+    return { msg: true, user };
+  }
+
+  @Post("/google")
+  public async authGoogle(@Req() req: any) {
+    const authCode = req.headers["auth-code"];
+    const { id_token, access_token, refresh_token } = await Await(
+      `${google.getAccess}?code=${authCode}&client_id=${process.env.GOOGLE_CLIENT_ID}&client_secret=${process.env.GOOGLE_CLIENT_SECRET}&redirect_uri=postmessage&grant_type=authorization_code`,
+      Option.post
+    );
+    const { sub, email, name, picture } = await Await(
+      `${google.getUserInfo}?id_token=${id_token}`,
+      Option.get
+    );
+    const accessToken = `google_${access_token}`;
+    const refreshToken = `google_${refresh_token}`;
+    if (await this.userService.checkDuplicate(sub)) {
+      const user = await this.userService.updateAuth(
+        sub,
+        name,
+        email,
+        picture,
+        accessToken,
+        refreshToken
+      );
+      return { msg: true, user };
+    }
+
+    const password = uuid();
+    const user = await this.userService.createAuth(
+      sub,
+      password,
+      name,
+      email,
+      picture,
+      accessToken,
+      refreshToken
+    );
+    return { msg: true, user };
+  }
+
+  @Get("/logout")
+  public async logout(@Req() req: any, @Res() res: any) {
+    // const sess = req.session;
+    // if (sess.username) {
+    //   sess.destroy();
+    //   res.clearCookie("connect.sid");
+    //   return true;
+    // }
+    // return false;
+  }
+}
---
+++ server/src/controllers/api/UserController.ts
@@ -0,0 +1,197 @@
+import {
+  JsonController,
+  Get,
+  Param,
+  Post,
+  Body,
+  BodyParam,
+  Patch,
+  Put,
+  Delete,
+  Req,
+  Res
+} from "routing-controllers";
+import { UserService } from "../../services/UserService";
+import { Users } from "../../models/Users";
+import jwt from "jsonwebtoken";
+import { UserDTO } from "../../dto/UserDTO";
+import { Await, Async, Option } from "../../util/fetchUtil";
+import { kakao, google } from "../../constants/oauthAPIs";
+import { keyValue2Str } from "../../util/StringUtils";
+
+/** TypeDi Constructor Injection 작동 방식
+ * 1. TypeDi의 Container를 routing-controllers가 사용한다.(server.ts 소스 코드 참조)
+ * 2. TypeDi가 Controller로 등록된 클래스들을 알고 있다.
+ * 3. TypeDi는 @Inject를 통해서 dependency를 주입하는데, 생성자 주입(Constructor Injection)의 경우 @Inject를 생략해도 된다.
+ */
+@JsonController("/users")
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  public async getUser(@Req() req: any, @Res() res: any) {
+    let accessToken = req.headers["access-token"];
+    let refreshToken = req.headers["refresh-token"];
+    if (accessToken.includes("kakao_") || refreshToken.includes("kakao_")) {
+      return await this.getUserKakao(accessToken, refreshToken);
+    }
+    if (accessToken.includes("google_") || refreshToken.includes("google_")) {
+      return await this.getUserGoogle(accessToken, refreshToken);
+    }
+    try {
+      const decodedAccessToken = jwt.verify(
+        accessToken,
+        `${process.env.JWT_KEY}`
+      );
+      const user = await this.userService.findOne(
+        (<any>decodedAccessToken).loginId
+      );
+      return user;
+    } catch {
+      try {
+        const decodedRefreshToken = jwt.verify(
+          refreshToken,
+          `${process.env.JWT_KEY}`
+        );
+        const { loginId } = <any>decodedRefreshToken;
+        accessToken = await jwt.sign({ loginId }, `${process.env.JWT_KEY}`, {
+          expiresIn: "2h"
+        });
+        const user = await this.userService.updateToken(
+          loginId,
+          accessToken,
+          refreshToken
+        );
+        return user;
+      } catch {
+        return false;
+      }
+    }
+  }
+
+  @Get("/:id")
+  public async findOne(@Param("id") loginId: string) {
+    const user = await this.userService.findOne(loginId);
+    return user;
+  }
+
+  @Post()
+  public async create(
+    @BodyParam("id") loginId: string,
+    @BodyParam("password") password: string,
+    @BodyParam("name") name: string,
+    @BodyParam("email") email: string,
+    @Req() req: any
+  ) {
+    //TODO: user을 Users Model에 맞게 class-transformer를 사용해서 처리하자
+    if (await this.userService.checkDuplicate(loginId)) {
+      return { msg: false, user: null };
+    }
+
+    const accessToken = await jwt.sign({ loginId }, `${process.env.JWT_KEY}`, {
+      expiresIn: "2h"
+    });
+
+    const refreshToken = await jwt.sign({ loginId }, `${process.env.JWT_KEY}`, {
+      expiresIn: "3 days"
+    });
+
+    const user = await this.userService.create(
+      loginId,
+      password,
+      name,
+      email,
+      accessToken,
+      refreshToken
+    );
+
+    return { msg: true, user };
+  }
+
+  @Put("/:id")
+  @Patch("/:id")
+  public update(@Param("id") id: string, @Body() user: Users) {
+    //TODO: user을 Users Model에 맞게 class-transformer를 사용해서 처리하자
+    // return this.userService.update(parseInt(id), user);
+  }
+
+  @Delete("/:id")
+  public delete(@Param("id") id: string) {
+    return this.userService.delete(parseInt(id));
+  }
+
+  public async getUserKakao(accessToken: string, refreshToken: string) {
+    const user = await this.userService.findOneByToken(accessToken);
+    accessToken = accessToken.replace("kakao_", "");
+    refreshToken = refreshToken.replace("kakao_", "");
+    const checkExpireResult = await Await(kakao.checkTokenExpired, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    const { expiresInMillis } = checkExpireResult;
+    if (expiresInMillis <= 0) {
+      const params = {
+        grant_type: "refresh_token",
+        client_id: process.env.KAKAO_API_KEY,
+        refresh_token: refreshToken
+      };
+      const paramsStr = keyValue2Str(params);
+      const { access_token, refresh_token } = await Await(
+        `${kakao.getToken}?${paramsStr}`,
+        Option.post
+      );
+      if (refresh_token !== undefined) refreshToken = `kakao_${refresh_token}`;
+      const { loginId, name, email, profileUrl } = <UserDTO>user;
+      return await this.userService.updateAuth(
+        loginId,
+        name,
+        email,
+        profileUrl,
+        `kakao_${access_token}`,
+        `kakao_${refreshToken}`
+      );
+    }
+
+    return user;
+  }
+
+  public async getUserGoogle(accessToken: string, refreshToken: string) {
+    const user = await this.userService.findOneByToken(accessToken);
+    accessToken = accessToken.replace("google_", "");
+    refreshToken = refreshToken.replace("google_", "");
+    const params = {
+      access_token: accessToken
+    };
+    const paramsStr = keyValue2Str(params);
+    const checkExpireResult = await Await(
+      `${google.checkTokenExpired}?${paramsStr}`,
+      Option.get
+    );
+    const { user_id } = checkExpireResult;
+    console.log(checkExpireResult);
+
+    if (user_id === undefined) {
+      const params = {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        refresh_token: refreshToken,
+        grant_type: "refresh_token"
+      };
+      const paramsStr = keyValue2Str(params);
+      const { access_token } = await Await(
+        `${google.getToken}?${paramsStr}`,
+        Option.post
+      );
+      const { loginId, name, email, profileUrl } = <UserDTO>user;
+      return await this.userService.updateAuth(
+        loginId,
+        name,
+        email,
+        profileUrl,
+        `google_${access_token}`,
+        `google_${refreshToken}`
+      );
+    }
+
+    return user;
+  }
+}
