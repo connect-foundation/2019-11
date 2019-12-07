34ba57a22a0ac8613cdcafd0e11809e53248ef04
Girlfirend-Yerin <hsunj2003@naver.com>
2019-12-07 16:03:11 +0900

    Update MyItem page
    아이템 삭제 전 AlertDialog가 등장한다.
    삭제 역할도 Page에서 일임해 RegisterPage와 일관적인 구조를 가진다.
    삭제 버튼을 누르면 순간 Item의 Id를 저장한 뒤 지운다.

--- client/src/pages/MyItems/index.jsx
+++ client/src/pages/MyItems/index.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useContext, useRef, useReducer } from "react"
+import React, { useState, useContext, useRef } from "react"
 import styled from "styled-components"
 
 import Header from "../../components/Atoms/Header"
 import TradeBox from "../../components/Organisim/TradeBox"
 import InfiniteScroll from "../../components/Molecules/InfiniteScroll"
 import Footer from "../../components/Atoms/Footer"
+import AlertDialog from "../../components/Molecules/AlertDialog"
 
 import userContext from "../../context/UserContext"
 
 import apiConfig from "../../config/api"
 import pathConfig from "../../config/path"
-import { getFetch } from "../../services/fetchService"
+import { getFetch, deleteJsonFetch } from "../../services/fetchService"
 
 import { limits } from "./contants"
 
@@ -51,7 +52,24 @@ const Page = () => {
   const [user] = useContext(userContext)
   const [offset, setOffset] = useState(0)
   const [hasMore, setHasMore] = useState(true)
-  const reset = useRef(false)
+  const [refState, refreshTrigger] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [removeId, setRemoveId] = useState(-1)
+
+  const refresh = () => {
+    setOffset(0)
+    setHasMore(true)
+    refreshTrigger(true)
+    refreshTrigger(false)
+  }
+
+  const removeHandler = async () => {
+    const result = await deleteJsonFetch(`${apiUrl}${products}/${removeId}`)
+    if (result.data.affected) {
+      alert("성공적으로 삭제되었습니다.")
+      refresh()
+    } else alert("이미 삭제 되었거나 경매 완료된 물품입니다.")
+  }
 
   const fetcher = async () => {
     const fetchUrl = `${apiUrl}${products}/onlySale/${user.id}/${offset}/${limits}`
@@ -68,7 +86,11 @@ const Page = () => {
         status: "경매중",
         thumbnail: value.thumbnailUrl,
         price: value.immediatePrice,
-        time: new window.Date(value.registerDate)
+        time: new window.Date(value.registerDate),
+        onDelete: () => {
+          setOpen(true)
+          setRemoveId(value.id)
+        }
       }
     })
   }
@@ -80,15 +102,24 @@ const Page = () => {
       <ContentContainer>
         <Header text={"경매중인 내 상품"} />
         <ScrollFrame>
-          <InfiniteScroll
-            fetcher={fetcher}
-            drawer={drawer}
-            hasMore={hasMore}
-            reset={reset.current}
-          />
+          <InfiniteScroll fetcher={fetcher} drawer={drawer} hasMore={hasMore} refresh={refState} />
         </ScrollFrame>
       </ContentContainer>
       <Footer />
+      {open ? (
+        <AlertDialog
+          title={"주의"}
+          content={"경매중인 상품은 취소시 매너지수가 하락합니다. 취소 하시겠습니까?"}
+          cancelAble={true}
+          onDismiss={() => {
+            setOpen(false)
+            setRemoveId(-1)
+          }}
+          onAccept={() => removeHandler()}
+        />
+      ) : (
+        undefined
+      )}
     </Container>
   )
 }
