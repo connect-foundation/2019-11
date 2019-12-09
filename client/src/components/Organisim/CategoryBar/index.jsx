a1845751f2961b3e77fd2fed12144321e2ad26cf
sphong5911 <sphong5911@naver.com>
2019-12-09 19:08:57 +0900

    Fix categorybar onBlur handler
    컴포넌트가 로드되지않았을때 클릭할 경우 ref로 지정한 node가
    undefined되어 발생하는 오류 수정

--- client/src/components/Organisim/CategoryBar/index.jsx
+++ client/src/components/Organisim/CategoryBar/index.jsx
@@ -1,78 +1,89 @@
-import React, { useState, useRef, useEffect, useContext } from "react"
-import Logo from "./Logo"
-import CategoryIcon from "./CategoryIcon"
-import ExpandList from "./ExpandList"
-import LoginButton from "./LoginButton"
-import Profile from "./Profile"
-import MainModal from "../../Molecules/MainModal"
-import Cloth from "../../../assets/cloth.svg"
-import Electronic from "../../../assets/television.svg"
-import LifeStyle from "../../../assets/geek.svg"
-import MessengerIcon from "../../../assets/messenger.svg"
-import detailCategoryList from "../../../data/detail-category-list"
-import { Container, OriginWrapper, ListWrapper, Bar, List, DivisionLine } from "./CategoryBarStyle"
-import userContext from "../../../context/UserContext"
-import Messenger from "../../Messenger"
-import apiConfig from "../../../config/api"
-import pathConfig from "../../../config/path"
-import { getFetch } from "../../../services/fetchService"
+import React, { useState, useRef, useEffect, useContext } from "react";
+import Logo from "./Logo";
+import CategoryIcon from "./CategoryIcon";
+import ExpandList from "./ExpandList";
+import LoginButton from "./LoginButton";
+import Profile from "./Profile";
+import MainModal from "../../Molecules/MainModal";
+import Cloth from "../../../assets/cloth.svg";
+import Electronic from "../../../assets/television.svg";
+import LifeStyle from "../../../assets/geek.svg";
+import MessengerIcon from "../../../assets/messenger.svg";
+import detailCategoryList from "../../../data/detail-category-list";
+import {
+  Container,
+  OriginWrapper,
+  ListWrapper,
+  Bar,
+  List,
+  DivisionLine
+} from "./CategoryBarStyle";
+import userContext from "../../../context/UserContext";
+import Messenger from "../../Messenger";
+import apiConfig from "../../../config/api";
+import pathConfig from "../../../config/path";
+import { getFetch } from "../../../services/fetchService";
 
-const { apiUrl } = apiConfig
-const { users } = pathConfig
+const { apiUrl } = apiConfig;
+const { users } = pathConfig;
 
 const Components = () => {
-  const [open, setOpen] = useState(false)
-  const [loginOpen, setLoginOpen] = useState(false)
-  const [selectIdx, setSelectIdx] = useState(1)
-  const [user, setUser] = useContext(userContext)
+  const [open, setOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [selectIdx, setSelectIdx] = useState(1);
+  const [user, setUser] = useContext(userContext);
 
-  const node = useRef()
+  const node = useRef();
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleOnBlur)
-    const refreshToken = localStorage.getItem("refresh-token")
-    const accessToken = localStorage.getItem("access-token")
-    if (refreshToken !== null && accessToken !== null) {
-      const headers = {
-        "access-token": `${accessToken}`,
-        "refresh-token": `${refreshToken}`
+    document.addEventListener("mousedown", handleOnBlur);
+    if (user.id === undefined) {
+      const refreshToken = localStorage.getItem("refresh-token");
+      const accessToken = localStorage.getItem("access-token");
+      if (refreshToken !== null && accessToken !== null) {
+        const headers = {
+          "access-token": `${accessToken}`,
+          "refresh-token": `${refreshToken}`
+        };
+        (async () => {
+          const result = await getFetch(`${apiUrl}${users}`, headers, {});
+          if (result) {
+            setUser(result);
+            localStorage.setItem("access-token", result.accessToken);
+            localStorage.setItem("refresh-token", result.refreshToken);
+          } else alert("세션이 만료되어 로그아웃됩니다.");
+        })();
       }
-      ;(async () => {
-        const result = await getFetch(`${apiUrl}${users}`, headers, {})
-        if (result) {
-          setUser(result)
-          localStorage.setItem("access-token", result.accessToken)
-          localStorage.setItem("refresh-token", result.refreshToken)
-        } else alert("세션이 만료되어 로그아웃됩니다.")
-      })()
     }
-  }, [])
+  }, []);
 
   const handleClick = e => {
-    const { idx } = e.target.dataset
+    const { idx } = e.target.dataset;
     if (selectIdx === idx || open === false) {
-      setOpen(!open)
+      setOpen(!open);
     }
-    setSelectIdx(idx)
-  }
+    setSelectIdx(idx);
+  };
 
   const handleLoginClick = () => {
-    setLoginOpen(!loginOpen)
-  }
+    setLoginOpen(!loginOpen);
+  };
 
   const handleLoginClose = () => {
-    loginOpen === true && setLoginOpen(!loginOpen)
-  }
+    loginOpen === true && setLoginOpen(!loginOpen);
+  };
 
   const handleOnBlur = e => {
-    if (!node.current.contains(e.target)) {
-      setOpen(false)
+    if (node.current !== (undefined || null)) {
+      if (!node.current.contains(e.target)) {
+        setOpen(false);
+      }
     }
-  }
+  };
 
   const close = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   return (
     <Container ref={node}>
@@ -111,7 +122,11 @@ const Components = () => {
               idx={3}
             />
           </List>
-          {user.isLogin === true ? <Messenger img={MessengerIcon} onClick={close} /> : undefined}
+          {user.isLogin === true ? (
+            <Messenger img={MessengerIcon} onClick={close} />
+          ) : (
+            undefined
+          )}
         </Bar>
       </OriginWrapper>
       <ListWrapper open={open}>
@@ -124,7 +139,7 @@ const Components = () => {
       </ListWrapper>
       <MainModal onClose={handleLoginClose} open={loginOpen} />
     </Container>
-  )
-}
+  );
+};
 
-export default Components
+export default Components;
