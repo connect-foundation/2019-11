d912eb9a493e6e787bb347700083b09e226d9c2b
Girlfirend-Yerin <hsunj2003@naver.com>
2019-12-09 17:04:37 +0900

    Update Products page
    상품 정보시 부가 정보에 대한 API분리에 따른 업데이트
    Bid를 추가해서 가져오는 API로 경로 변경
    unused Property 삭제

--- client/src/pages/Product/index.jsx
+++ client/src/pages/Product/index.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import ProductInfo from "../../components/Organisim/ProductInfo";
-import ChatBox from "../../components/Organisim/Chat/ChatBox";
-import AuctionGraph from "../../components/Organisim/AuctionGraph";
-import Spinner from "../../components/Atoms/Spinner";
-import { useFetch } from "../../hooks/useFetch";
-import pathConfig from "../../config/path";
+import React, { useState } from "react"
+import styled from "styled-components"
+import ProductInfo from "../../components/Organisim/ProductInfo"
+import ChatBox from "../../components/Organisim/Chat/ChatBox"
+import AuctionGraph from "../../components/Organisim/AuctionGraph"
+import Spinner from "../../components/Atoms/Spinner"
+import { useFetch } from "../../hooks/useFetch"
+import pathConfig from "../../config/path"
 
 const ProductPageStyle = styled.div`
   display: flex;
@@ -13,28 +13,28 @@ const ProductPageStyle = styled.div`
   width: 100%;
   max-width: 1440px;
   padding: var(--padding-md);
-`;
+`
 
 const MainColumn = styled.div`
   flex: 1;
   overflow-x: auto;
   overflow-x: hidden;
-`;
+`
 
 const TextStyle = styled.p`
   font-size: ${props => props.size};
-`;
+`
 
 const Section = styled.section`
   min-height: 400px;
   margin-bottom: var(--margin-xl);
   display: flex;
   ${props => (props.center ? "justify-content: center" : undefined)}
-`;
+`
 
 const ChatColumn = styled.div`
   width: 400px;
-`;
+`
 
 const Loading = styled.div`
   width: 100%;
@@ -43,17 +43,16 @@ const Loading = styled.div`
   justify-content: center;
   align-items: center;
   border: 1px solid red;
-`;
+`
 
 const user = {
   id: "chsch1028",
   src: "https://i.pravatar.cc/150?img=4"
-};
+}
 
 const ProductPage = ({ match }) => {
-  const productId = match.params.id;
-  const [product, setProduct] = useState({});
-  const fetchState = useFetch(`${pathConfig.products}/${productId}`);
+  const productId = match.params.id
+  const fetchState = useFetch(`${pathConfig.productsWithBids}/${productId}`)
 
   return fetchState.loading ? (
     <Spinner />
@@ -71,7 +70,7 @@ const ProductPage = ({ match }) => {
         <ChatBox productId={productId} user={user}></ChatBox>
       </ChatColumn>
     </ProductPageStyle>
-  );
-};
+  )
+}
 
-export default ProductPage;
+export default ProductPage
