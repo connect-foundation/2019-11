1f3f12d64db3dbeb9ec53b1b1cd0571f7a50de15
sphong5911 <sphong5911@naver.com>
2019-12-10 18:20:12 +0900

    Update sign up dialog
    회원가입 유효성 검사 과정 추가
    tooltip을 이용하여 유효성 조건을 보여주도록 추가

--- client/src/components/Molecules/MainModal/SignUpDialog.jsx
+++ client/src/components/Molecules/MainModal/SignUpDialog.jsx
@@ -2,11 +2,13 @@ import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
+import ReactTooltip from "react-tooltip";
 import { DialogStyle, Input, SubmitButton } from "./LoginDialogStyles";
 import UserContext from "../../../context/UserContext";
 import apiConfig from "../../../config/api";
 import pathConfig from "../../../config/path";
 import { postJsonFetch } from "../../../services/fetchService";
+import { validate } from "./constant";
 const { apiUrl } = apiConfig;
 const { users } = pathConfig;
 const InputContainer = styled.div`
@@ -22,44 +24,101 @@ const Label = styled.label`
 const SignUpDialog = ({ close }) => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
-  const [checkPwd, setCheckPwd] = useState("");
+  const [retypePwd, setRetypePwd] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [user, setUser] = useContext(UserContext);
+  const [isValidateId, setIsValidateId] = useState(false);
+  const [isValidatePwd, setIsValidatePwd] = useState(false);
+  const [isValidateRetypePwd, setIsValidateRetypePwd] = useState(false);
+  const [isValidateEmail, setIsValidateEmail] = useState(false);
+  const [isValidateName, setIsValidateName] = useState(false);
+
+  const checkIdIsValidate = text => {
+    setIsValidateId(validate.id.test(text));
+  };
+
+  const checkPwdIsValidate = text => {
+    const { length, alphabet, number } = validate.password;
+    if (!length.test(text) || !alphabet.test(text) || !number.test(text))
+      setIsValidatePwd(false);
+    else setIsValidatePwd(true);
+  };
+
+  const checkRetypePwdValidate = text => {
+    setIsValidateRetypePwd(password === text);
+  };
+
+  const checkEmailValidate = text => {
+    setIsValidateEmail(validate.email.test(text));
+  };
+  const checkNameValidate = text => {
+    setIsValidateName(text !== "");
+  };
+
   const handleIdKeyUp = e => {
     setId(e.target.value);
+    checkIdIsValidate(e.target.value);
   };
   const handlePasswordKeyUp = e => {
     setPassword(e.target.value);
+    checkPwdIsValidate(e.target.value);
   };
-  const handleCheckPwdKeyUp = e => {
-    setCheckPwd(e.target.value);
+  const handleRetypePwdKeyUp = e => {
+    setRetypePwd(e.target.value);
+    checkRetypePwdValidate(e.target.value);
   };
   const handleNameKeyUp = e => {
     setName(e.target.value);
+    checkNameValidate(e.target.value);
   };
   const handleEmailKeyUp = e => {
     setEmail(e.target.value);
+    checkEmailValidate(e.target.value);
   };
+
+  const checkIsValidateAll = () => {
+    return (
+      isValidateId &&
+      isValidatePwd &&
+      isValidateRetypePwd &&
+      isValidateName &&
+      isValidateEmail
+    );
+  };
+
+  const handleOnBlurIdInput = () => {};
+
   const handleSubmit = async e => {
     e.preventDefault();
-    const body = {
-      id,
-      password,
-      name,
-      email
-    };
-    const result = await postJsonFetch(`${apiUrl}${users}`, {}, body);
-    const { msg, user } = result;
-    if (msg) {
-      setUser(user);
-      localStorage.setItem("access-token", user.accessToken);
-      localStorage.setItem("refresh-token", user.refreshToken);
-      alert("회원가입 완료");
+    if (!checkIsValidateAll()) {
+      const alertMsg = `다음 입력이 올바르지 않습니다.\n${
+        !isValidateId ? "- 아이디\n" : ""
+      }${!isValidatePwd ? "- 비밀번호\n" : ""}${
+        !isValidateRetypePwd ? "- 비밀번호 재입력\n" : ""
+      }${!isValidateName ? "- 이름\n" : ""}${
+        !isValidateEmail ? "- 이메일" : ""
+      }`;
+      alert(alertMsg);
     } else {
-      alert("회원가입에 실패하였습니다.");
+      const body = {
+        id,
+        password,
+        name,
+        email
+      };
+      const result = await postJsonFetch(`${apiUrl}${users}`, {}, body);
+      const { msg, user } = result;
+      if (msg) {
+        setUser(user);
+        localStorage.setItem("access-token", user.accessToken);
+        localStorage.setItem("refresh-token", user.refreshToken);
+        alert("회원가입 완료");
+        close();
+      } else {
+        alert("이미 존재하는 아이디입니다.");
+      }
     }
-    close();
   };
   return (
     <DialogStyle>
@@ -67,13 +126,16 @@ const SignUpDialog = ({ close }) => {
         <InputContainer>
           <Label>아이디</Label>
           <Input
+            data-tip="5-20자의 영문,숫자로 작성"
             type="text"
             name="id"
             placeholder="ID"
             onKeyUp={handleIdKeyUp}
+            onBlur={handleOnBlurIdInput}
           />
           <Label>비밀번호</Label>
           <Input
+            data-tip="8-16자의 영어,숫자,특수문자를 조합하여 작성"
             type="password"
             name="password"
             placeholder="PASSWORD"
@@ -84,7 +146,7 @@ const SignUpDialog = ({ close }) => {
             type="password"
             name="check-password"
             placeholder="Retype PASSWORD"
-            onKeyUp={handleCheckPwdKeyUp}
+            onKeyUp={handleRetypePwdKeyUp}
           />
           <Label>이름</Label>
           <Input
@@ -100,6 +162,7 @@ const SignUpDialog = ({ close }) => {
             placeholder="E-MAIL"
             onKeyUp={handleEmailKeyUp}
           />
+          <ReactTooltip place="right" effect="solid" />
         </InputContainer>
       </DialogContent>
       <DialogActions>
