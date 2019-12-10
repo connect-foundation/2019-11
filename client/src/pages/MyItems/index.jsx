65707e8d4ea2ae9925a92808a3a952aa8eceb179
Girlfirend-Yerin <hsunj2003@naver.com>
2019-12-10 13:22:23 +0900

    Fix MyItems page
    인피니티 스크롤이 정상적으로 작동하지 않던 문제 해결
    state가 아닌 ref로 처리 했어야함

--- client/src/pages/MyItems/index.jsx
+++ client/src/pages/MyItems/index.jsx
@@ -1,22 +1,23 @@
-import React, { useState, useContext, useRef } from "react"
-import styled from "styled-components"
+import React, { useState, useContext, useRef, useEffect } from "react";
+import styled from "styled-components";
 
-import Header from "../../components/Atoms/Header"
-import TradeBox from "../../components/Organisim/TradeBox"
-import InfiniteScroll from "../../components/Molecules/InfiniteScroll"
-import Footer from "../../components/Atoms/Footer"
-import AlertDialog from "../../components/Molecules/AlertDialog"
+import Header from "../../components/Atoms/Header";
+import TradeBox from "../../components/Organisim/TradeBox";
+import InfiniteScroll from "../../components/Molecules/InfiniteScroll";
+import Footer from "../../components/Atoms/Footer";
+import AlertDialog from "../../components/Molecules/AlertDialog";
+import Spinner from "../../components/Atoms/Spinner";
 
-import userContext from "../../context/UserContext"
+import userContext from "../../context/UserContext";
 
-import apiConfig from "../../config/api"
-import pathConfig from "../../config/path"
-import { getFetch, deleteJsonFetch } from "../../services/fetchService"
+import apiConfig from "../../config/api";
+import pathConfig from "../../config/path";
+import { getFetch, deleteJsonFetch } from "../../services/fetchService";
 
-import { limits } from "./contants"
+import { limits } from "./contants";
 
-const { url, apiUrl } = apiConfig
-const { products } = pathConfig
+const { url, apiUrl } = apiConfig;
+const { products } = pathConfig;
 
 const Container = styled.div`
   width: 100%;
@@ -24,7 +25,7 @@ const Container = styled.div`
   overflow-y: auto;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const ContentContainer = styled.div`
   display: flex;
@@ -36,7 +37,7 @@ const ContentContainer = styled.div`
   margin: 5px auto;
   border-radius: 30px;
   overflow-y: auto;
-`
+`;
 
 const ScrollFrame = styled.div`
   width: 60rem;
@@ -46,36 +47,43 @@ const ScrollFrame = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
-`
+`;
 
 const Page = () => {
-  const [user] = useContext(userContext)
-  const [offset, setOffset] = useState(0)
-  const [hasMore, setHasMore] = useState(true)
-  const [refState, refreshTrigger] = useState(false)
-  const [open, setOpen] = useState(false)
-  const [removeId, setRemoveId] = useState(-1)
+  const [user] = useContext(userContext);
+  const offset = useRef(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [refState, refreshTrigger] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [removeId, setRemoveId] = useState(-1);
+  const [onlazy, setOnlazy] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOnlazy(false);
+    }, 1000);
+  });
 
   const refresh = () => {
-    setOffset(0)
-    setHasMore(true)
-    refreshTrigger(true)
-    refreshTrigger(false)
-  }
+    offset.current = 0;
+    setHasMore(true);
+    refreshTrigger(true);
+    refreshTrigger(false);
+  };
 
   const removeHandler = async () => {
-    const result = await deleteJsonFetch(`${apiUrl}${products}/${removeId}`)
+    const result = await deleteJsonFetch(`${apiUrl}${products}/${removeId}`);
     if (result.data.affected) {
-      alert("성공적으로 삭제되었습니다.")
-      refresh()
-    } else alert("이미 삭제 되었거나 경매 완료된 물품입니다.")
-  }
+      alert("성공적으로 삭제되었습니다.");
+      refresh();
+    } else alert("이미 삭제 되었거나 경매 완료된 물품입니다.");
+  };
 
   const fetcher = async () => {
-    const fetchUrl = `${apiUrl}${products}/onlySale/${user.id}/${offset}/${limits}`
-    const [list, cnt] = await getFetch(fetchUrl)
-    setOffset(offset + list.length)
-    setHasMore(offset.current < cnt)
+    const fetchUrl = `${apiUrl}${products}/onlySale/${user.id}/${offset.current}/${limits}`;
+    const [list, cnt] = await getFetch(fetchUrl);
+    offset.current += list.length;
+    setHasMore(offset.current < cnt);
 
     return list.map(value => {
       return {
@@ -88,17 +96,19 @@ const Page = () => {
         price: value.immediatePrice,
         time: new window.Date(value.registerDate),
         onDelete: () => {
-          setOpen(true)
-          setRemoveId(value.id)
+          setOpen(true);
+          setRemoveId(value.id);
         },
         onUpdate: () => (window.location = `/productUpdate/${value.id}`)
-      }
-    })
-  }
+      };
+    });
+  };
 
-  const drawer = item => item.map(value => <TradeBox {...value} />)
+  const drawer = item => item.map(value => <TradeBox {...value} />);
 
-  return (
+  return onlazy ? (
+    <Spinner />
+  ) : (
     <Container>
       <ContentContainer>
         <Header text={"경매중인 내 상품"} />
@@ -113,8 +123,8 @@ const Page = () => {
           content={"경매중인 상품은 취소시 매너지수가 하락합니다. 취소 하시겠습니까?"}
           cancelAble={true}
           onDismiss={() => {
-            setOpen(false)
-            setRemoveId(-1)
+            setOpen(false);
+            setRemoveId(-1);
           }}
           onAccept={() => removeHandler()}
         />
@@ -122,7 +132,7 @@ const Page = () => {
         undefined
       )}
     </Container>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
