f65fac0f940d5b1ac1db19285d3db140a50865d4
Girlfirend-Yerin <hsunj2003@naver.com>
2019-12-12 18:15:57 +0900

    Remove components warning
    성능 문제를 초래할 수 있는 경고 제거
    key 및 display 속성이 겹치는 부분 제거

--- client/src/components/Molecules/SelectBox/List/index.jsx
+++ client/src/components/Molecules/SelectBox/List/index.jsx
@@ -1,7 +1,7 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
-import ListItem from "../ListItem"
+import ListItem from "../ListItem";
 
 const Container = styled.div`
   width: 100%;
@@ -12,35 +12,36 @@ const Container = styled.div`
   border-radius: inherit;
 
   ${props => {
-    if (props.open) return `border: #aaaaaa solid 1px;`
+    if (props.open) return `border: #aaaaaa solid 1px;`;
   }}
-`
+`;
 
 const Wrapper = styled.div`
   width: 100%;
   height: ${props => props.count * 2}rem;
   overflow-y: auto;
   overflow-x: hidden;
-`
+`;
 
 const Component = props => {
-  const { open, show, selected, list, handler } = props
+  const { open, show, selected, list, handler } = props;
 
   return (
     <Container open={open} count={show < list.length ? show : list.length}>
       <Wrapper count={show < list.length ? show : list.length}>
         {list.map((value, idx) => (
           <ListItem
+            key={idx}
             text={value}
             selected={selected === idx}
             onClick={ev => {
-              handler(idx)
+              handler(idx);
             }}
           />
         ))}
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Component
+export default Component;
