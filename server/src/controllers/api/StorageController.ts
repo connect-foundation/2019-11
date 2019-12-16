e60a20ff242f2df4bd391756f64838bce6d1a604
sphong5911 <sphong5911@naver.com>
2019-12-16 11:35:41 +0900

    Update profile storage API
    프로필 이미지 등록 API를 등록 후 사용자 정보를 보내주도록 수정

--- server/src/controllers/api/StorageController.ts
+++ server/src/controllers/api/StorageController.ts
@@ -10,19 +10,22 @@ import {
 } from "routing-controllers";
 
 import s3 from "../../services/S3Service";
+import { UserService } from "../../services/UserService";
 
 @JsonController("/storage")
 @ContentType("image/*")
 @OnUndefined(403)
 @OnNull(403)
 export class StoreageController {
+  constructor(private readonly userService: UserService) {}
   @Post("/image")
   public async image(
     @HeaderParam("x-auth") uid: string,
     @HeaderParam("x-timestamp") timestamp: string,
     @BodyParam("uri") data: string
   ) {
-    if (uid === undefined || timestamp === undefined) return new ForbiddenError();
+    if (uid === undefined || timestamp === undefined)
+      return new ForbiddenError();
 
     const result = s3.creatObject("image/", data);
     return result;
@@ -32,11 +35,14 @@ export class StoreageController {
   public async profile(
     @HeaderParam("x-auth") uid: string,
     @HeaderParam("x-timestamp") timestamp: string,
-    @BodyParam("uri") data: string
+    @BodyParam("uri") data: string,
+    @BodyParam("id") id: number
   ) {
-    if (uid === undefined || timestamp === undefined) return new ForbiddenError();
+    if (uid === undefined || timestamp === undefined)
+      return new ForbiddenError();
 
-    const result = s3.creatObject("profile/", data);
-    return result;
+    const result = await s3.creatObject("profile/", data);
+    const user = this.userService.updateUserProfile(id, result);
+    return user;
   }
 }
