bd67cf0f4482eea5699424a5ed844b955bca17d1
sphong5911 <sphong5911@naver.com>
2019-12-17 15:56:42 +0900

    Update static controller
    Json controller의 명칭이 Storage Controller로 되어있어 파일명과
    일치하도록 Static controller로 변경

--- server/src/controllers/api/StaticController.ts
+++ server/src/controllers/api/StaticController.ts
@@ -2,7 +2,7 @@ import { JsonController, Get } from "routing-controllers";
 import categoryJson from "../../constants/category.json";
 
 @JsonController("/statics")
-export class StorageController {
+export class StaticController {
   @Get("/categories")
   public categories() {
     return categoryJson;
